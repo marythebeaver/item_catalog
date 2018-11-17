aed30e80ee86c84eb5258f207b5f267b4a9390de
vagrant <vagrant@vagrant.vm>
2018-11-17 18:26:41 +0000

    feat: fake data

--- category.py
+++ category.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base
+
+catagory = {'id': '1', 'name': 'Independent Watchmaker'}
+catagories = [{'id': '1', 'name': 'Independent Watchmaker'}, {
+    'id': '2', 'name': 'Manufactor Watch Brand'}]
+items = [{'cat_id': '1', 'id': '1', 'brand': 'Akrivia', 'description': 'Founded by Reshep'}, {
+    'cat_id': '1', 'id': '2', 'brand': 'Kari Voutilainen', 'description': 'Founded by Kari'}, {
+    'cat_id': '2', 'id': '1', 'brand': 'Patek Philippe', 'description': 'Founded by Antoni Patek and Adrien Philippe'}, {
+    'cat_id': '2', 'id': '2', 'brand': 'Rolex', 'description': 'Founded by Hans Wilsdorf'}]
